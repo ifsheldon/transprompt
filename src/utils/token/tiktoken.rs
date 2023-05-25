f3066927a67c976f9d6f9af05bcb724ee8d0d73e
Feng Liang <feng.liang@kaust.edu.sa>
2023-05-26 00:31:32 +0800

    remove unnecessary mod definition

--- src/utils/token/tiktoken.rs
+++ src/utils/token/tiktoken.rs
@@ -1,30 +1,28 @@
-pub mod tiktoken {
-    pub use tiktoken_rs::*;
-    use anyhow::Result;
-    use crate::utils::token::CountToken;
+pub use tiktoken_rs::*;
+use anyhow::Result;
+use crate::utils::token::CountToken;
 
-    #[derive(Clone)]
-    #[readonly::make]
-    pub struct Tiktoken {
-        #[readonly]
-        pub model: String,
-        #[readonly]
-        pub bpe: CoreBPE,
+#[derive(Clone)]
+#[readonly::make]
+pub struct Tiktoken {
+    #[readonly]
+    pub model: String,
+    #[readonly]
+    pub bpe: CoreBPE,
+}
+
+impl Tiktoken {
+    pub fn new(model: impl Into<String>) -> Result<Self> {
+        let model = model.into();
+        get_bpe_from_model(&model).and_then(|bpe| Ok(Tiktoken {
+            model,
+            bpe,
+        }))
     }
+}
 
-    impl Tiktoken {
-        pub fn new(model: impl Into<String>) -> Result<Self> {
-            let model = model.into();
-            get_bpe_from_model(&model).and_then(|bpe| Ok(Tiktoken {
-                model,
-                bpe,
-            }))
-        }
-    }
-
-    impl CountToken for Tiktoken {
-        fn count_token(&self, string: &str) -> usize {
-            self.bpe.encode_with_special_tokens(string).len()
-        }
+impl CountToken for Tiktoken {
+    fn count_token(&self, string: &str) -> usize {
+        self.bpe.encode_with_special_tokens(string).len()
     }
 }
